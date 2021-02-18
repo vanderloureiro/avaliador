83d28caedd0c9bd9c912dcce7fddb300b38ab36a
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-02-17 21:54:36 -0300

    Primeiro commit

---
+++ src/main/java/com/br/avaliador/AvaliadorApplication.java
@@ -0,0 +1,13 @@
+package com.br.avaliador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/avaliador/AvaliadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.avaliador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
